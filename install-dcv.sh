e7183fbd3ba9c70dc5913ce435a979832f2c2a6f
Nigel Gardiner <ngl@amazon.com>
2023-02-24 14:39:12 +1100

    removed old code

--- install-dcv.sh
+++ install-dcv.sh
@@ -25,36 +25,6 @@ sudo apt-get install -y ./nice-dcv-web-viewer_*.deb
 usermod -aG video dcv
 sudo apt-get install -y ./nice-xdcv_*.deb
 
-#if [ ${ROSVERSION} == "ROS1Melodic" ] || [ ${ROSVERSION} == "NoROSUbuntu1804" ]; then
-## get_dcv_pkg
-#wget https://d1uj6qtbmh3dt5.cloudfront.net/2022.2/Servers/nice-dcv-2022.2-14521-ubuntu1804-x86_64.tgz
-#tar -xvzf nice-dcv-2022.2-14521-ubuntu1804-x86_64.tgz && rm nice-dcv-2022.2-14521-ubuntu1804-x86_64.tgz
-#
-## install_dcv
-#echo Installing DCV for Ubuntu 18.04
-#cd nice-dcv-2022.2-14521-ubuntu1804-x86_64 && \
-#DEBIAN_FRONTEND=noninteractive apt-get install -y \
-#./nice-dcv-server_2022.2.14521-1_amd64.ubuntu1804.deb \
-#./nice-xdcv_2022.2.519-1_amd64.ubuntu1804.deb
-#
-#elif [ ${ROSVERSION} == "ROS1Noetic" ] || [ ${ROSVERSION} == "NoROSUbuntu2004" ]; then
-#echo Installing DCV for Ubuntu 20.04
-#wget https://d1uj6qtbmh3dt5.cloudfront.net/2022.2/Servers/nice-dcv-2022.2-14521-ubuntu2004-x86_64.tgz
-#tar xvfz nice-dcv-2022.2-14521-ubuntu2004-x86_64.tgz
-#cd nice-dcv-2022.2-14521-ubuntu2004-x86_64
-#DEBIAN_FRONTEND=noninteractive apt-get install -y \
-#./nice-dcv-server_2022.2.14521-1_amd64.ubuntu2004.deb \
-#./nice-xdcv_2022.2.14521-1_amd64.ubuntu2004.deb
-#else
-#echo Installing DCV for Ubuntu 22.04
-#wget https://d1uj6qtbmh3dt5.cloudfront.net/2022.2/Servers/nice-dcv-2022.2-14521-ubuntu2204-x86_64.tgz
-#tar xvfz nice-dcv-2022.2-14521-ubuntu2204-x86_64.tgz
-#cd nice-dcv-2022.2-14521-ubuntu2204-x86_64
-#DEBIAN_FRONTEND=noninteractive apt-get install -y \
-#./nice-dcv-server_2022.2.14521-1_amd64.ubuntu2204.deb \
-#./nice-xdcv_2022.2.519-1_amd64.ubuntu2204.deb
-#fi
-
 cd /home/ubuntu
 
 # create_dcv_conf
